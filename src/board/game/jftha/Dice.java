38cd09c7c312470e2490ebada4a1fd0c4a1873c7
tuetran <tua84076@temple.edu>
2014-03-14 17:28:46 -0400

    Update Dice.java

--- src/board/game/jftha/Dice.java
+++ src/board/game/jftha/Dice.java
@@ -12,7 +12,7 @@ public class Dice {
     //Constructor
     public Dice(){
         Random rand = new Random();
-        this.roll = rand.nextInt(4) + 1;
+        this.roll = rand.nextInt(4) + 1; // rand.nextInt(4) is random from 0 - 3. + 1 ensure it can be 1- 4
     }
     
     protected int getRoll(){
